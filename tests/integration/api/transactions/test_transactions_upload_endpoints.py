6fc041ad23acc6bf679b5543d36e10157ba1e7d0
Patryk  Staszczuk <pstaszczuk@MacBook-Air-Patryk.local>
2025-07-27 21:10:58 +0200

    chore: uploading transactions api test now checks if transactions are created

--- tests/integration/api/transactions/test_transactions_upload_endpoints.py
+++ tests/integration/api/transactions/test_transactions_upload_endpoints.py
@@ -3,9 +3,11 @@ from unittest.mock import patch
 
 import pytest
 from fastapi.testclient import TestClient
+from sqlalchemy import select
 
 from app.core.media import get_media_dir
 from app.main import app
+from app.transactions.models import Transaction
 from tests.integration.api.common import override_deps
 
 client = TestClient(app)
@@ -13,12 +15,15 @@ client = TestClient(app)
 
 @pytest.mark.usefixtures("celery_session_app")
 @pytest.mark.usefixtures("celery_session_worker")
+@patch("app.transactions.tasks.get_session")
 def test_transactions_upload_should_save_file_to_future_processing_return_201_and_invoke_celery_task(
+    get_session_mock,
     db_session,
     tmp_path,
 ) -> None:
     override_deps(db_session)
     app.dependency_overrides[get_media_dir] = lambda: str(tmp_path / "media")
+    get_session_mock.side_effect = lambda: iter([db_session])
 
     fake_file = BytesIO(
         b"transaction_id,timestamp,amount,currency,customer_id,product_id,quantity\n"
@@ -28,18 +33,17 @@ def test_transactions_upload_should_save_file_to_future_processing_return_201_an
 
     fake_file.name = "test.csv"
 
-    with patch("app.api.transactions.endpoints.process_transactions_file_local.delay") as mock_celery_task:
-        response = client.post(
-            app.url_path_for("transactions-upload") + "?delimiter=,",
-            files={"file": ("test.csv", fake_file, "text/csv")},
-        )
-        response_json = response.json()
-        saved_file_path = tmp_path / "media" / "transactions" / f"{response_json['import_id']}.csv"
+    response = client.post(
+        app.url_path_for("transactions-upload") + "?delimiter=,",
+        files={"file": ("test.csv", fake_file, "text/csv")},
+    )
+    response_json = response.json()
+    saved_file_path = tmp_path / "media" / "transactions" / f"{response_json['import_id']}.csv"
 
-        assert response.status_code == 201
-        assert "import_id" in response_json
-        mock_celery_task.assert_called_once()
-        assert saved_file_path.exists()
+    assert response.status_code == 201
+    assert "import_id" in response_json
+    assert saved_file_path.exists()
+    assert len(db_session.execute(select(Transaction)).all()) == 2
 
 
 def test_should_return_400_if_file_has_invalid_header(db_session, tmp_path) -> None:
