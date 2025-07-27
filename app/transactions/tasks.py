52d33a8f42897e311ffbb0f0ccf64090a75dfc6c
Patryk  Staszczuk <pstaszczuk@MacBook-Air-Patryk.local>
2025-07-27 20:52:26 +0200

    feat: saving transactions into database

--- app/transactions/tasks.py
+++ app/transactions/tasks.py
@@ -1,7 +1,24 @@
-from celery import shared_task
+from typing import Iterator
 
-from app.transactions.uploads.transactions_file_processor import TransactionsFileProcessor
+from celery import shared_task
+from sqlalchemy.dialects.postgresql import insert as pg_insert
+from sqlalchemy.orm import Session
+
+from app.core.session import get_session as get_session_global
+from app.transactions.models import Transaction as TransactionORMModel
+from app.transactions.uploads.transactions_file_processor import (
+    Transaction,
+    TransactionRowError,
+    TransactionsFileProcessor,
+)
 from app.transactions.uploads.transactions_file_reader import StandardLocalTransactionsCSVFileReader
+
+
+def get_session() -> Iterator[Session]:
+    yield from get_session_global()
+
+
+BATCH_SIZE = 10000
 
 
 @shared_task
@@ -9,4 +26,32 @@ def process_transactions_file_local(path: str, delimiter: str) -> None:
     processor = TransactionsFileProcessor(
         file_reader=StandardLocalTransactionsCSVFileReader(path, delimiter=delimiter),
     )
-    processor.process_file()
+    session = next(get_session())
+
+    batch = []
+
+    for result in processor.process_file():
+        if isinstance(result, TransactionRowError):
+            continue
+        batch.append(result)
+
+        if len(batch) >= BATCH_SIZE:
+            _insert_batch(session, batch)
+            batch.clear()
+
+    if batch:
+        _insert_batch(session, batch)
+
+    session.commit()
+
+
+def _insert_batch(session, transactions: list[Transaction]) -> None:
+    stmt = (
+        pg_insert(TransactionORMModel)
+        .values(
+            [t.model_dump() for t in transactions],
+        )
+        .on_conflict_do_nothing(index_elements=["transaction_id"])
+    )
+
+    session.execute(stmt)
