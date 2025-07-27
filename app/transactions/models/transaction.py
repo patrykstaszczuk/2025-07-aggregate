d55ff069defd2f114a8a6e9a4682a60caa1056b7
Patryk  Staszczuk <pstaszczuk@MacBook-Air-Patryk.local>
2025-07-27 17:18:45 +0200

    chore: add indexes for columns in Transaction model

--- app/transactions/models/transaction.py
+++ app/transactions/models/transaction.py
@@ -2,7 +2,7 @@ from datetime import datetime, timezone
 from uuid import UUID as UUIDType
 from uuid import uuid4
 
-from sqlalchemy import DateTime, Float, Integer, String
+from sqlalchemy import DateTime, Float, Index, Integer, String
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import Mapped, mapped_column
 
@@ -11,11 +11,16 @@ from app.core.session import Base
 
 class Transaction(Base):
     __tablename__ = "transactions"
+    __table_args__ = (Index("idx_transactions_customer_product", "customer_id", "product_id"),)
 
     transaction_id: Mapped[UUIDType] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid4)
-    timestamp: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))
+    timestamp: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        default=lambda: datetime.now(timezone.utc),
+        index=True,
+    )
     amount: Mapped[float] = mapped_column(Float)
-    currency: Mapped[str] = mapped_column(String(3))
-    customer_id: Mapped[UUIDType] = mapped_column(UUID(as_uuid=True))
-    product_id: Mapped[UUIDType] = mapped_column(UUID(as_uuid=True))
+    currency: Mapped[str] = mapped_column(String(3), index=True)
+    customer_id: Mapped[UUIDType] = mapped_column(UUID(as_uuid=True), index=True)
+    product_id: Mapped[UUIDType] = mapped_column(UUID(as_uuid=True), index=True)
     quantity: Mapped[int] = mapped_column(Integer)
